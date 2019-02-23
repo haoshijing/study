d80c12147e8f18c92f68bcb0ae4422a53ca97996
郝仕兢 <haoshijing690@163.com>
2019-02-23 16:21:17 +0800

    导入修改

--- src/main/java/com/nc/study/controller/DataController.java
+++ src/main/java/com/nc/study/controller/DataController.java
@@ -3,18 +3,11 @@ package com.nc.study.controller;
 
 import com.nc.study.po.DataPo;
 import com.nc.study.service.DataService;
-import com.nc.study.util.ExcelUtil;
-import jdk.internal.util.xml.impl.Input;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.io.InputStream;
-import java.util.List;
 
 @Controller
 @RequestMapping("/data")
